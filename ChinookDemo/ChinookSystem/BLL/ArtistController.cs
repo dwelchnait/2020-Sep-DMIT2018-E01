370d170fd6ce0c54a4c707096cee8a3f7d882e61
Don Welch <dwelch@nait.ca>
2020-09-16 20:44:37 -0600

    Commerce creating the filter search web page, create the BLL ArtistController and ViewModel SelectionList classes

---
+++ ChinookDemo/ChinookSystem/BLL/ArtistController.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+#region Additional Namespaces
+using ChinookSystem.Entities;
+using ChinookSystem.DAL;
+using ChinookSystem.ViewModels;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    public class ArtistController
+    {
+        //public List<SelectionList> Artists_List()
+        //{
+        //    using(var context = new ChinookSystemContext())
+        //    {
+        //        return context.Artists.ToList();
+        //    }
+        //}
+    }
+}
---
+++ ChinookDemo/ChinookSystem/ViewModels/SelectionList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.ViewModels
+{
+    public class SelectionList
+    {
+        //use this class to carry data for a drop down list
+        //a drop down list needs a value and a display string
+        public int ValueId { get; set; }
+        public string DisplayText { get; set; }
+    }
+}
