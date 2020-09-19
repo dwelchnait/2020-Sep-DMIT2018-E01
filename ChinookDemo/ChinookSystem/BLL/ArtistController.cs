898035a94b3fdac4d5f57ed651d6e88f3c030105
Don Welch <dwelch@nait.ca>
2020-09-18 20:04:54 -0600

    BLL Artist controller method ArtistList() using Linq and view model pattern. ASPX form SearchByDDL dropdown list.

--- ChinookDemo/ChinookSystem/BLL/ArtistController.cs
+++ ChinookDemo/ChinookSystem/BLL/ArtistController.cs
@@ -16,12 +16,39 @@ namespace ChinookSystem.BLL
 {
     public class ArtistController
     {
-        //public List<SelectionList> Artists_List()
-        //{
-        //    using(var context = new ChinookSystemContext())
-        //    {
-        //        return context.Artists.ToList();
-        //    }
-        //}
+        public List<SelectionList> Artists_List()
+        {
+            //due to the fact that the entities wil be internal
+            //  you will NOT be able to use the entity definitions (classes)
+            //  as the return datatypes
+
+            //instead, we will create views within the ViewModel classes that will
+            //  contain the data definitions for your return datatypes
+
+            //to fill these view model classes, we will use Linq queries
+            //Linq quereis return their data as IEnumerable or IQueryable datasets
+            //you can use var when declaring your query recieving variables
+            //the recieving variables can then be converted to a List<T>
+            //this Linq query below uses the query syntax method
+
+            using (var context = new ChinookSystemContext())
+            {
+                //Linq to Entity queries
+                //where is the access to the application library system entities
+                //they are in your context class ChinookSystemContext -> context
+                //the entities are accessed by your DbSet<T> -> Artists
+                //x represents any occurance (instance) in the DbSet<T>
+                var results = from x in context.Artists
+                              select new SelectionList
+                              {
+                                  ValueId = x.ArtistId,
+                                  DisplayText = x.Name
+                              };
+                //sort the dataset by a specified property
+                return results.OrderBy(x => x.DisplayText).ToList();
+
+                //return context.Artists.ToList();  // in CPSC1517 entities were public
+            }
+        }
     }
 }
